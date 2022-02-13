48ead9a5062f4fc2c56293f494d9782e7161a04f
Jorge Silva <jorge.silva>
2022-02-13 19:44:30 -0300

    header

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ const Nav = ({ categories }) => {
           <ul className="uk-navbar-nav">
             <li>
               <Link href="/">
-                <a>Strapi Blog</a>
+                <a>Jorge Mendes</a>
               </Link>
             </li>
           </ul>
